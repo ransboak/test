d3663d6195ca7ac4e694a47352460e315ff0ca84
ransboak <66210638+ransboak@users.noreply.github.com>
2024-01-18 08:35:42 -0800

    Welcome page changes

--- routes/web.php
+++ routes/web.php
@@ -18,22 +18,17 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    $posts = [];
-    if(auth()->check()){
-        $posts = auth()->user()->usersCoolPosts()->latest()->get();
-    }
-    
     //$posts = Post::where('user_id', auth()->id())->get();
-    return view('home', ['posts' => $posts]);
+    return view('welcome');
 });
 
-Route::post('/register', [UserController::class, 'register']);
+// Route::post('/register', [UserController::class, 'register']);
 
-Route::post('/logout', [UserController::class, 'logout']);
-Route::post('/login', [UserController::class, 'login']);
+// Route::post('/logout', [UserController::class, 'logout']);
+// Route::post('/login', [UserController::class, 'login']);
 
 
-Route::post('/create-post', [PostController::class, 'createPost']);
-Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
-Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
-Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
+// Route::post('/create-post', [PostController::class, 'createPost']);
+// Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+// Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
+// Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
